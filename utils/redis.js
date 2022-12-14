e617e8ac7c5ba91d1f45fd7f61f26650356b0d4a
Bilkis Ismail <bilkisismail07@gmail.com>
2022-12-14 17:39:52 +0700

    Update redis.js

--- utils/redis.js
+++ utils/redis.js
@@ -4,7 +4,11 @@ const Redis = require("ioredis");
 let redis;
 
 if (process.env.REDIS) {
-  redis = new Redis();
+  redis = new Redis({
+    host: 'redis-15140.c9.us-east-1-4.ec2.cloud.redislabs.com',
+    port: 15140,
+    password: '33u5L2WZb2zT4PFgPQZ1k9S3IeFTraQV'
+  });
 }
 
 module.exports = redis;
